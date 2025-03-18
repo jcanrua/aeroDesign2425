fb15b8a6890d756cf2d8b7fd0f78d73f535ba537
sgilleo <sergiogilleon2004@gmail.com>
2025-03-18 16:27:35 +0100

    Added mavlink inspector with pymavlink

--- mavlinkInspector.py
+++ mavlinkInspector.py
@@ -2,9 +2,12 @@
 
 from pymavlink import mavutil
 
-connection = mavutil.mavlink_connection("udpin:127.0.0.1:14550", baud = 115200) #mavutil.mavlink_connection(device = "COM7", baud = 57600)
+print("Connecting")
 
+connection = mavutil.mavlink_connection("/dev/serial0", baud = 115200)
 connection.wait_heartbeat()
+
+print("Vehicle connected")
 
 while True:
 	msg = connection.recv_match(blocking = True)
