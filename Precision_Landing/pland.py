818f73be5c4568ddea2d1e5063d3830c3a9aebc2
sgilleo <sergiogilleon2004@gmail.com>
2025-03-18 12:02:45 +0100

    PLAND DEFINITIVO

--- Precision_Landing/pland.py
+++ Precision_Landing/pland.py
@@ -3,15 +3,17 @@ import cv2.aruco as aruco
 from picamera2 import Picamera2
 import numpy as np
 import time
-import dronekit
-import pymavlink
+from pymavlink import mavutil
+from datetime import datetime
+import os
+import logging
 
 aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_ARUCO_ORIGINAL)
 parameters = aruco.DetectorParameters()
 detector = aruco.ArucoDetector(aruco_dict, parameters)
 
-id_to_find = 72
-takeoff_height = 5
+viewVideo = True
+
 
 horizontal_res = 640
 vertical_res = 480
@@ -23,131 +25,128 @@ picam2.start()
 horizontal_fov = 62.2 * (np.pi / 180 ) ##Pi cam V1: 53.5 V2: 62.2
 vertical_fov = 48.8 * (np.pi / 180)    ##Pi cam V1: 41.41 V2: 48.8
 
-calib_path="calibrationFiles/"
-cameraMatrix   = np.loadtxt(calib_path+'cameraMatrix.txt', delimiter=',')
-cameraDistortion   = np.loadtxt(calib_path+'cameraDistortion.txt', delimiter=',')
-
 ##Counters and script triggers
 found_count=0
 notfound_count=0
-
 first_run=0 #Used to set initial time of function to determine FPS
 start_time=0
 end_time=0
-script_mode = 1 ##1 for arm and takeoff, 2 for manual LOITER to GUIDED land 
-ready_to_land=0 ##1 to trigger landing
 
-def arm_and_takeoff(targetHeight):
-    while vehicle.is_armable!=True:
-        print("Waiting for vehicle to become armable.")
-        time.sleep(1)
-    print("Vehicle is now armable")
-
-    vehicle.mode = dronekit.VehicleMode("GUIDED")
-            
-    while vehicle.mode!='GUIDED':
-        print("Waiting for drone to enter GUIDED flight mode")
-        time.sleep(1)
-    print("Vehicle now in GUIDED MODE. Have fun!!")
-
+# Set up logging configuration
+def setup_logger():
+    # Get current time and format it for use in filename
+    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    log_filename = f"log_vuelo_{current_time}.txt"
     
-    vehicle.armed = True
-    while vehicle.armed==False:
-        print("Waiting for vehicle to become armed.")
-        time.sleep(1)
+    logging.basicConfig(
+        filename=log_filename,
+        level=logging.INFO,  # Can also use DEBUG for more detailed logs
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+
+# Utility function to log and print messages
+def log_and_print(message, is_error=False):
+    if is_error:
+        print(f"ERROR: {message}")
+        logging.error(message)
+    else:
+        print(message)
+        logging.info(message)
+
+
+def send_land_message(marker_id,corners):
+    x_sum = corners[marker_id][0][0][0]+ corners[marker_id][0][1][0]+ corners[marker_id][0][2][0]+ corners[marker_id][0][3][0]
+    y_sum = corners[marker_id][0][0][1]+ corners[marker_id][0][1][1]+ corners[marker_id][0][2][1]+ corners[marker_id][0][3][1]
     
-    print("Look out! Props are spinning!!")
+    x_avg = x_sum*.25
+    y_avg = y_sum*.25
             
-    vehicle.simple_takeoff(targetHeight) ##meters
+    x_ang = (x_avg - horizontal_res*.5)*(horizontal_fov/horizontal_res)
+    y_ang = (y_avg - vertical_res*.5)*(vertical_fov/vertical_res)
+            
+    msg = vehicle.mav.landing_target_send(
+        0,
+        0,
+        0,
+        x_ang,
+        y_ang,
+        0,
+        0,
+        0
+    )
 
-    while True:
-        print("Current Altitude: %d"%vehicle.location.global_relative_frame.alt)
-        if vehicle.location.global_relative_frame.alt>=.95*targetHeight:
-            break
-        time.sleep(1)
-    print("Target altitude reached!!")
-
-def send_land_message(x,y):
-    msg = vehicle.message_factory.landing_target_encode(
-        0,
-        0,
-        pymavlink.mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,
-        x,
-        y,
-        0,
-        0,
-        0,)
-    vehicle.send_mavlink(msg)
-    vehicle.flush()
+    return x_avg, y_avg
 
 def lander():
-    global first_run,notfound_count,found_count,marker_size,start_time
+    global first_run,notfound_count,found_count,start_time
     if first_run==0:
-        print("First run of lander!!")
+        log_and_print("First run of lander!!")
         first_run=1
         start_time=time.time()
         
     frame = picam2.capture_array()
     frame = cv2.resize(frame,(horizontal_res,vertical_res))
-    #frame_np = np.array(frame)
     gray_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    ids=''
+
+
     corners, ids, rejected = detector.detectMarkers(image=gray_img)
-    if vehicle.mode!='LAND':
-        vehicle.mode=dronekit.VehicleMode("LAND")
-        while vehicle.mode!='LAND':
-            print('WAITING FOR DRONE TO ENTER LAND MODE')
-            time.sleep(1)
+ 
     try:
-        if ids is not None and ids[0] == id_to_find:
+        if ids is not None:
+
+            x_avg, y_avg = send_land_message(0, corners)
             
-            y_sum = 0
-            x_sum = 0
-            
-            x_sum = corners[0][0][0][0]+ corners[0][0][1][0]+ corners[0][0][2][0]+ corners[0][0][3][0]
-            y_sum = corners[0][0][0][1]+ corners[0][0][1][1]+ corners[0][0][2][1]+ corners[0][0][3][1]
-    
-            x_avg = x_sum*.25
-            y_avg = y_sum*.25
-            
-            x_ang = (x_avg - horizontal_res*.5)*(horizontal_fov/horizontal_res)
-            y_ang = (y_avg - vertical_res*.5)*(vertical_fov/vertical_res)
-            
-            if vehicle.mode!='LAND':
-                vehicle.mode = dronekit.VehicleMode('LAND')
-                while vehicle.mode!='LAND':
-                    time.sleep(1)
-                print("------------------------")
-                print("Vehicle now in LAND mode")
-                print("------------------------")
-                send_land_message(x_ang,y_ang)
-            else:
-                send_land_message(x_ang,y_ang)
-                pass
-            print("X CENTER PIXEL: "+str(x_avg)+" Y CENTER PIXEL: "+str(y_avg))
-            print("FOUND COUNT: "+str(found_count)+" NOTFOUND COUNT: "+str(notfound_count))
+            if viewVideo==True:
+                aruco.drawDetectedMarkers(frame, corners)
+                cv2.circle(frame,(x_avg, y_avg), 10, (255, 0, 0), -1)                  
+
+            log_and_print("X CENTER PIXEL: "+str(x_avg)+" Y CENTER PIXEL: "+str(y_avg))
+            log_and_print("FOUND COUNT: "+str(found_count)+" NOTFOUND COUNT: "+str(notfound_count))
             found_count=found_count+1
-            print("")
         else:
             notfound_count=notfound_count+1
     except Exception as e:
-        print('Target likely not found. Error: '+str(e))
+        log_and_print('Target likely not found. Error: '+str(e))
         notfound_count=notfound_count+1
 
+    if viewVideo==True:
+        cv2.imshow("frame", frame)
+
+######################################################
+
+#######################MAIN###########################
+
+######################################################
+
+setup_logger()
+
+log_and_print("Connecting to vehicle...")
+
+vehicle = mavutil.mavlink_connection("/dev/serial0", baud = 115200)
+
+vehicle.wait_heartbeat()
+
+log_and_print("Connected to vehicle")
+
+#vehicle.parameters['PLND_ENABLED'] = 1
+#vehicle.parameters['PLND_TYPE'] = 1 ##1 for companion computer
+#vehicle.parameters['PLND_EST_TYPE'] = 0 ##0 for raw sensor, 1 for kalman filter pos estimation
+#vehicle.parameters['LAND_SPEED'] = 20 ##Descent speed of 30cm/s
+
+#STUFF HERE
+#---------------
+
+#---------------
 
 
-vehicle = dronekit.connect("tcp:127.0.0.1:5762",wait_ready=True)
+#Esperar a que el avion este en modo VTOL_LANDING
 
-vehicle.parameters['PLND_ENABLED'] = 1
-vehicle.parameters['PLND_TYPE'] = 1 ##1 for companion computer
-vehicle.parameters['PLND_EST_TYPE'] = 0 ##0 for raw sensor, 1 for kalman filter pos estimation
-vehicle.parameters['LAND_SPEED'] = 20 ##Descent speed of 30cm/s
-
-arm_and_takeoff(takeoff_height)
-
-print("Despegue completado, esperando 5 segundos")
-
-time.sleep(5)
-
-while vehicle.armed==True:
+log_and_print("Starting landing...")
+while True:
     lander()
+
+
+#STUFF HERE
+#---------------
+
+#---------------
